24e634381f3121a0fe5eb08686f58f6b826289ab
fedor.soloviev <fedor.soloviev>
2018-12-04 19:14:47 +0300

    Add project 2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.upButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numeric = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(580, 426);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellValueChanged);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.DataGridView1_SelectionChanged);
+            // 
+            // upButton
+            // 
+            this.upButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.upButton.Location = new System.Drawing.Point(656, 49);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(133, 43);
+            this.upButton.TabIndex = 2;
+            this.upButton.Text = "Увеличить";
+            this.upButton.UseVisualStyleBackColor = true;
+            this.upButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // downButton
+            // 
+            this.downButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.downButton.Location = new System.Drawing.Point(656, 131);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(133, 43);
+            this.downButton.TabIndex = 3;
+            this.downButton.Text = "Уменьшить";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(684, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "С шагом";
+            // 
+            // numeric
+            // 
+            this.numeric.DecimalPlaces = 3;
+            this.numeric.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numeric.Location = new System.Drawing.Point(669, 259);
+            this.numeric.Name = "numeric";
+            this.numeric.Size = new System.Drawing.Size(120, 26);
+            this.numeric.TabIndex = 5;
+            this.numeric.ThousandsSeparator = true;
+            this.numeric.ValueChanged += new System.EventHandler(this.NumericValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(875, 450);
+            this.Controls.Add(this.numeric);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.upButton);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Редактор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numeric;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,20 +1,187 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using SolidEdgeFramework;
+using System.Runtime.InteropServices;
+using SolidEdge.Part.Variables;
+using System.Drawing;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private enum ColumnType { name, value };
+
+        private double step = 0.01;
+
+        private int selectedRow = 0;
+
+        SolidEdgeFramework.Application application = null;
+        SolidEdgeFramework.Documents documents = null;
+        SolidEdgeFramework.SolidEdgeDocument document = null;
+        SolidEdgeFramework.Variables variables = null;
+        VariableList variableList = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[(int)ColumnType.name].Name = @"Имя";
+            dataGridView1.Columns[(int)ColumnType.value].Name = @"Значение";
+            dataGridView1.Columns[(int)ColumnType.name].ReadOnly = true;
+            dataGridView1.Columns[(int)ColumnType.value].ReadOnly = false;
+            dataGridView1.Columns[(int)ColumnType.name].ValueType = typeof(string);
+            dataGridView1.Columns[(int)ColumnType.value].ValueType = typeof(double);
+            dataGridView1.Columns[(int)ColumnType.value].DefaultCellStyle.Format = "N3";
+
+            numeric.Value = (decimal)step;
+
+            ConnectToSolid();
+            UpdateVariables();
+        }
+
+        private void UpdateVariables()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+            variables = (Variables)document.Variables;
+
+            variableList = (VariableList)variables
+            .Query(pFindCriterium: "*", 
+            NamedBy: SolidEdgeConstants.VariableNameBy.seVariableNameByBoth,
+            VarType: SolidEdgeConstants.VariableVarType.SeVariableVarTypeBoth);
+
+            dynamic variableListItem = null;
+
+            for (int i = 1; i <= variableList.Count; i++)
+            {
+                variableListItem = variableList.Item(i);
+                dynamic f = variableListItem.Formula.Length == 0;
+                string value = variableListItem.Value.ToString("0.000"); ;
+                object[] row = { variableListItem.DisplayName, variableListItem.Value };
+                dataGridView1.Rows.Add(row);
+                dataGridView1.Rows[i - 1].DefaultCellStyle.BackColor = (variableListItem.Formula.Length == 0 ? Color.White : Color.Coral);
+            }
+        }
+
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            double newValue = (double)dataGridView1.Rows[e.RowIndex].Cells[(int)ColumnType.value].Value;
+            dynamic variableListItem = variableList.Item(selectedRow + 1);
+            variableListItem.Value = newValue;
+        }
+
+        private void DataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            selectedRow = dataGridView1.CurrentCell.RowIndex;
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            dynamic variableListItem = variableList.Item(selectedRow + 1);
+            UpdateValue(variableListItem.Value + step);
+        }
+
+        private void DownButton_Click(object sender, EventArgs e)
+        {
+            dynamic variableListItem = variableList.Item(selectedRow + 1);
+            if (variableListItem.Value - step >= 0)
+            {
+                UpdateValue(variableListItem.Value - step);
+            }
+        }
+
+        private void UpdateValue(double newValue)
+        {
+            DataGridViewCell cell = dataGridView1.Rows[selectedRow].Cells[(int)ColumnType.value];
+            cell.Value = newValue;
+        }
+
+        private void NumericValueChanged(object sender, EventArgs e)
+        {
+            step = decimal.ToDouble(((NumericUpDown)sender).Value);
+        }
+
+        private void ConnectToSolid()
+        {
+            try
+            {
+                OleMessageFilter.Register();
+                application = ConnectToSolidEdge(true);
+                application.Visible = true;
+                application.Activate();
+
+                // Get a reference to the Documents collection.
+                documents = application.Documents;
+
+                application.Activate();
+                // This check is necessary because application.ActiveDocument will throw an
+                // exception if no documents are open...
+                if (documents.Count > 0)
+                {
+                    // Attempt to connect to ActiveDocument.
+                    document = (SolidEdgeFramework.SolidEdgeDocument)application.ActiveDocument;
+                }
+
+                // Make sure we have a document.
+                if (document == null)
+                {
+                    throw new Exception("No active document.");
+                }
+
+                variables = (Variables)document.Variables;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                OleMessageFilter.Revoke();
+            }
+        }
+
+        /// <summary>
+        /// Connects to a running instance of Solid Edge with an option to start if not running.
+        /// </summary>
+        public static SolidEdgeFramework.Application ConnectToSolidEdge(bool startIfNotRunning)
+        {
+            try
+            {
+                // Attempt to connect to a running instance of Solid Edge.
+                return (SolidEdgeFramework.Application)
+                    Marshal.GetActiveObject("SolidEdge.Application");
+            }
+            catch (System.Runtime.InteropServices.COMException ex)
+            {
+                // Failed to connect.
+                if (ex.ErrorCode == -2147221021 /* MK_E_UNAVAILABLE */)
+                {
+                    if (startIfNotRunning)
+                    {
+                        // Start Solid Edge.
+                        return (SolidEdgeFramework.Application)
+                            Activator.CreateInstance(Type.GetTypeFromProgID("SolidEdge.Application"));
+                    }
+                    else
+                    {
+                        throw new System.Exception("Solid Edge is not running.");
+                    }
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
